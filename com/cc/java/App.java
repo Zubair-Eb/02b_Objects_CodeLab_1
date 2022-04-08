3e06811db48c677649b7528deb24c2a01cd3bc6a
Zubair-Eb <zubair.yousaf-eberl@gmx.de>
2022-04-08 16:45:00 +0200

    Done

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,6 +3,46 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
+
+        Konto konto= new Konto(10000, 10000*2);
+        Konto konto2= new Konto(10000, 10000*3);
+        Konto konto3= new Konto(10000, 10000*10);
+
+
+        output("-----Aufgabe-1-----");
+
+        output("Anfangswert Konto 1:\t\t" + konto.Kontostand);
+        output("Anfangswert Konto 2:\t\t" + konto2.Kontostand);
+        output("Anfangswert Konto 3:\t\t" + konto3.Kontostand);
+
+        output("-----Aufgabe-2-----");
+
+        output("Verdoppelung Konto 1:\t\t" + konto.multiplier);
+        output("Verdreifachung Konto 2:\t\t" + konto2.multiplier);
+        output("Verzehnfachung5 Konto 3:\t\t" + konto3.multiplier);
+
+        output("-----Aufgabe-2-----");
+
+        output("Aktuellen Kontostand Konto 1:\t" + konto.multiplier);
+        output("Aktuellen Kontostand Konto 2:\t" + konto2.multiplier);
+        output("Aktuellen Kontostand Konto 3:\t" + konto3.multiplier);
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
+
+        
+
      
     }
 
---
+++ com/cc/java/Konto.java
@@ -0,0 +1,15 @@
+package com.cc.java;
+
+public class Konto {
+
+    public int Kontostand;
+    public int multiplier;
+
+    public Konto(int kontostand, int multiplier) {
+        this.Kontostand = kontostand;
+        this.multiplier = multiplier;
+    }
+
+    
+    
+}
